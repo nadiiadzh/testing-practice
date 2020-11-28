6ca9f67df1cc55e49996affe472911245c6d6a94
Nadiia Dzhyhryniuk <nadiadzh7@gmail.com>
2020-11-27 20:15:40 -0500

    sum tests created

--- 01-unit/01-sum.js
+++ 01-unit/01-sum.js
@@ -1,8 +1,11 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function sum() {
-
+function sum(num1, num2) {
+  
+	if (num1 === undefined) num1 = 0;
+	if (num2 === undefined) num2 = 0;
+	return num1 + num2
 }
 
 // ==================================================
@@ -20,16 +23,20 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
-
+  var result = sum(1,2,3,4,5);
+  if (result !== 3) throw new Error('Expected sum(1,2) to be 3. Received: ' + result);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+  var result = sum(3);
+  if (result !== 3) throw new Error('Expected sum(1) to be 3. Received: ' + result);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = sum();
+  if (result !== 0) throw new Error('Expected sum() to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
