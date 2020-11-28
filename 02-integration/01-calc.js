1db5f692c7247a24fcab9711621e5c74ee33c200
Nadiia Dzhyhryniuk <nadiadzh7@gmail.com>
2020-11-27 22:11:19 -0500

    calculator tests added

--- 02-integration/01-calc.js
+++ 02-integration/01-calc.js
@@ -1,7 +1,60 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function calc() {
+function sum(num1, num2) {
+
+	if (num1 === undefined) num1 = 0;
+  if (num2 === undefined) num2 = 0;
+  var $result = num1 + num2
+	return $result
+}
+function difference(num1, num2) {
+
+  if (num1 === undefined) num1 = 0;
+	if (num2 === undefined) num2 = 0;
+	var $result = num1 - num2
+	return $result
+}
+function product(num1, num2) {
+
+  if (num1 === undefined && num2 === undefined){
+    num1 = 0;
+    num2 = 0;
+  } 
+  else if (num1 === undefined){
+    num1 = 1;
+  }
+  else if (num2 === undefined){
+    num2 = 1;
+  }
+  var $result = num1 * num2
+	return $result
+}
+function quotient(num1, num2) {
+
+  if (num1 === undefined) num1 = 0; 
+  if (num2 === undefined) num2 = 1;
+  if (num2 === 0) return "ERROR";
+
+  var $result = num1 / num2
+	return $result
+}
+
+function calc(operation, num1, num2) {
+
+  switch(operation){
+
+    case 'add': return sum(num1, num2);
+
+    case 'subtract': return difference(num1, num2);
+
+    case 'multiply': return product(num1, num2);
+
+    case 'divide': return quotient(num1, num2);
+
+    default: return "Operation not supported."
+
+  }
 
 }
 
@@ -20,21 +73,28 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return the correct difference when the user provides: 'subtract', 20, 10.
-
+  var result = calc('subtract', 20, 10);
+  if (result !== 10) throw new Error('Expected calc("subtract", 20, 10) to be 10. Received: ' + result);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should return the correct product when the user provides: 'multiply', 9, 9.
-
+  var result = calc('multiply', 9, 9);
+  if (result !== 81) throw new Error('Expected calc("multiply", 9, 9) to be 81. Received: ' + result);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // It should return the correct quotient when the user provides: 'divide', 9, 3.
-
+  var result = calc('divide', 9, 3);
+  if (result !== 3) throw new Error('Expected calc("divide", 9, 3) to be 3. Received: ' + result);
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // It should return the message 'Operation not supported.' when the user provides: exponent, 2, 8.
+  var result = calc('exponent', 2, 8);
+  if (result !== 'Operation not supported.') throw new Error(`Expected calc("exponent", 2, 8) to be 'Operation not supported.'. Received: ${result}`);
+
+
 
   console.log('All tests passed successfully.');
 
